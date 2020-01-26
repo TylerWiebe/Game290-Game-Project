53dc44be9b72ccc01e84b05ad6d76ba8246a8721
DaneMunday <46771374+DaneMunday@users.noreply.github.com>
2020-01-25 22:19:30 -0800

    Camera Follow Script
    Script allowing for camera to slowly follow player
    
    Note:
    - Attach to Camera Object
    -Name Player Object "Player"

---
+++ Game290_ProjectAlpha/Assets/script_CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class script_CameraFollow : MonoBehaviour
+{
+    //Holds target position to move towards
+    private Vector3 targetPosition;
+
+    //holds camera position
+    private Vector3 currentPosition;
+
+    //holds the name of the player object the camera will follow
+    [SerializeField]
+    private string playerObjectName = "Player";
+
+    //holds camera follow speed
+    [SerializeField]
+    private int cameraFollowSpeed = 8;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        currentPosition = transform.position;
+
+        targetPosition.x = GameObject.Find(playerObjectName).transform.position.x;
+        targetPosition.y = GameObject.Find(playerObjectName).transform.position.y;
+        targetPosition.z = -10;
+
+
+        transform.position = Vector3.MoveTowards(currentPosition, targetPosition, (float) cameraFollowSpeed / 100);
+    }
+}
