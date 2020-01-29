09d371d87825684750db3c19e999e011b989b300
DaneMunday <46771374+DaneMunday@users.noreply.github.com>
2020-01-28 21:27:36 -0800

    Enemy AI and Enemy Alert Event Scripts
    Script_EnemyAI - Attach to enemy game object.
    
    ***Note - Player must be tagged with "Player" tag
    
    -------------------------------
    
    Script_EnemyAlert - Attach to enemy range game object
    
    ***Note - enemy mrange object must be a child of the "enemy" game object.

---
+++ Game290_ProjectAlpha/Assets/Scripts/Script_EnemyAI.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Controls Enemy behaviour for both passive and alerted phases.  
+//Enemy walks back and forth.  If player appears in sight, walk towards and attack player.
+//NOTE - Player game object must be tagged "Player"
+
+public class Script_EnemyAI : MonoBehaviour
+{
+    //Speed of walking after seeing player
+    [SerializeField]
+    private int aggressiveSpeed = 7;
+
+    //Speed of walking before seeing player
+    [SerializeField]
+    private int passiveSpeed = 5;
+
+    //Whether player has been spotted or not
+    public bool playerNotSeen = true;
+
+    //holds the game object enemy chases
+    private Transform target;
+
+    //direction enemy starts walking
+    [SerializeField]
+    private string walkingDirection = "Right";
+
+    [SerializeField]
+    private float roamDistance = 5f;
+
+    void Start ()
+    {
+        //assign the object with tag "Player" to be the enemy's target 
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerNotSeen == true)
+        {
+            StartCoroutine(PassiveBehaviour());
+        }
+
+        else
+        {
+            StartCoroutine(AggressiveBehaviour());
+        }
+    }
+
+
+
+
+    //Passive enemy behaviour (When enemy has not yet spotted player)
+    IEnumerator PassiveBehaviour()
+    {
+        //Walk right for specified seconds
+        while ((walkingDirection == "right") || (walkingDirection == "Right"))
+        {
+            transform.Translate(Vector2.right * passiveSpeed * Time.deltaTime);
+
+            //wait
+            yield return new WaitForSeconds(roamDistance);
+
+            //change direction
+            walkingDirection = "left";
+            yield break;
+        }
+
+        //walk
+        while ((walkingDirection == "left") || (walkingDirection == "Left"))
+        {
+            transform.Translate(Vector2.left * passiveSpeed * Time.deltaTime);
+
+            //wait
+            yield return new WaitForSeconds(roamDistance);
+
+            //change direction
+            walkingDirection = "right";
+            yield break;
+        }
+
+        //pause and turn right
+        yield return new WaitForSeconds(1);
+    }
+
+    //Agressive enemy behaviour triggered after enemy notices player
+    IEnumerator AggressiveBehaviour()
+    {
+        //pause and play alerted animation
+        yield return new WaitForSeconds(1);
+
+        //walk towards target position at specified speed
+        transform.position = Vector2.MoveTowards(transform.position, target.position, aggressiveSpeed * Time.deltaTime);
+    }
+}
---
+++ Game290_ProjectAlpha/Assets/Scripts/Script_EnemyAlert.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_EnemyAlert : MonoBehaviour
+{
+    //Attach this script to the enemy vision range game object.
+    //Note - the vision range game object must be a child of the "Enemy" game object
+
+    //on entering enemy alert range cause trigger event
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        //check if the collision was with the player
+        if ((other.tag == "player") || (other.tag == "Player"))
+        {
+            //change the value of the enemy's playerNotSeen value (swap to aggressive mode)
+            gameObject.GetComponentInParent<Script_EnemyAI>().playerNotSeen = false;
+        }
+    }
+}
