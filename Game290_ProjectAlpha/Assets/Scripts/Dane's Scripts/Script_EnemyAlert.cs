7b2c8dc9b6a394408aec614a16a2a18d343f72a5
DaneMunday <46771374+DaneMunday@users.noreply.github.com>
2020-01-29 13:36:48 -0800

    Enemy AI Scripts
    Attach Script_EnemyAI to enemy
    
    Attach Script_EnemyAlert to enemy alert range object
    
    Attach Script_EnemyAttack to enemy attack range object

---
+++ Game290_ProjectAlpha/Assets/Scripts/Dane's Scripts/Script_EnemyAlert.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_EnemyAlert : MonoBehaviour
+{
+    //Attach this script to the enemyVisionrRange game object.
+    //Note - the vision range game object must be a child of the "Enemy" game object
+
+    //holds enemy alerted icon
+    [SerializeField]
+    private GameObject myPrefab = null;
+
+    private Vector3 position;
+
+    //on entering enemy alert range cause trigger event
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        //check if the collision was with the player and player hasn't been seen by enemy before
+        if (((other.tag == "player") || (other.tag == "Player")) && (gameObject.GetComponentInParent<Script_EnemyAI>().playerNotSeen == true))
+        {
+            //create alert indicator above enemy's head
+            position = transform.position;
+            position.y += 3;
+            GameObject clone = Instantiate(myPrefab, position, Quaternion.identity);
+
+            //destroy clone after 1 second
+            Destroy(clone, 1.0f);
+
+            //change the value of the enemy's playerNotSeen value (swap to aggressive mode)
+            gameObject.GetComponentInParent<Script_EnemyAI>().playerNotSeen = false;
+        }
+    }
+}
---
+++ Game290_ProjectAlpha/Assets/Scripts/Dane's Scripts/Script_EnemyAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This script must be attached to the attackRange game object and must also be a child of the "enemy" game object.
+public class Script_EnemyAttack : MonoBehaviour
+{
+    //allows enemy to attack if set to "true"
+    private bool enemyInRange = false;
+
+    //when a collision with player occurs, trigger attacks
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        if ((other.tag == "player") || (other.tag == "Player"))
+        {
+            enemyInRange = true;
+
+            //when enemy is in range attack
+            while (enemyInRange == true)
+            {
+                Attack();
+                //attack every 1.5 seconds
+                yield return new WaitForSeconds(1.5f);
+            }
+        }
+    }
+
+
+
+
+    //trigger when player leaves enemy range (stop attacking)
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        enemyInRange = false;
+    }
+
+
+
+
+    //temporary attack function
+    private void Attack()
+    {
+        Debug.Log("Attack");
+    }
+}
