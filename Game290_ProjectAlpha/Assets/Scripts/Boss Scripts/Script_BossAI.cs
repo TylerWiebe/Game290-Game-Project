f92d23c5dec67595e4783ca82bbbf173b4f59540
Nathan Ahrendt <nathandfahrendt@gmail.com>
2020-03-06 00:16:24 -0800

    Implmented a boss prototype

---
+++ Game290_ProjectAlpha/Assets/Scripts/Boss Scripts/Script_BossAI.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Script_BossAI : MonoBehaviour
+{
+    //boss's rigidbody
+    private Rigidbody2D rigidBody;
+
+    //player game object
+    GameObject target = null;
+
+    //shotgun attack script
+    Script_Shotgun_Attack shotgun_attack;
+
+    //flame thrower attack script
+    Script_Flame_Thrower_Attack flame_thrower_attack;
+
+
+    //active weapon
+    /*
+     * Shotgun = 1
+     * Flamethrower = 2
+     * Laser = 3
+     */
+    private int weapon = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set rigidbody variable 
+        rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+
+        //set enemy game object variable
+        target = GameObject.FindGameObjectWithTag("Player");
+        
+        //check weapon change condition every 2.5 seconds
+        InvokeRepeating("chooseWeapon", 0f, 2.5f);
+
+        //set shotgun attack script
+        shotgun_attack = this.gameObject.GetComponentInChildren<Script_Shotgun_Attack>();
+        //set flame thrower attack script
+        flame_thrower_attack = this.gameObject.GetComponentInChildren<Script_Flame_Thrower_Attack>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate boss
+        rigidBody.rotation = calculateAngle();
+    }
+
+    //select which weapon to use based on player's distance from boss
+    private void chooseWeapon()
+    {
+        //flamethrower for close range
+        Vector3 targetPosition = target.transform.position;
+        Vector3 bossPosition = this.gameObject.transform.position;
+        if (Math.Pow(targetPosition.x - bossPosition.x, 2) + Math.Pow(targetPosition.y - bossPosition.y, 2) < 80) //range condition
+        {
+            weapon = 2;
+            shotgun_attack.stopAttack(); //stop attacking with the shotgun
+            flame_thrower_attack.startAttack(); //start attacking with the flame thrower
+        }
+        //shotgun for long range
+        else
+        {
+            weapon = 1;
+            flame_thrower_attack.stopAttack();  //stop attacking with the flame thrower
+            shotgun_attack.startAttack(); //start attacking with the shotgun
+        }
+
+    }
+
+    //calculate the angle of rotation paying respect to the offset shift
+    private float calculateAngle()
+    {
+        Vector3 targetPosition = target.transform.position;
+        targetPosition.x = targetPosition.x - this.gameObject.transform.position.x;
+        targetPosition.y = targetPosition.y - this.gameObject.transform.position.y;
+        return (Mathf.Atan2(targetPosition.y, targetPosition.x) * Mathf.Rad2Deg) - (weapon * 90);
+    }
+    
+}
---
+++ Game290_ProjectAlpha/Assets/Scripts/Boss Scripts/Script_Shotgun_Attack.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Script_Shotgun_Attack : MonoBehaviour
+{
+    //controls when the script can attack
+    private bool canAttack = false;
+
+    //player game object
+    GameObject target = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set enemy game object variable
+        target = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startAttack()
+    {
+        //start an attack, starts in 0.5 seconds, shoots every 5 seconds
+        InvokeRepeating("attack", 0.5f, 5f);
+    }
+
+    public void stopAttack()
+    {
+        //cancel attack
+        CancelInvoke();
+    }
+
+    //shot five projectiles in a line
+    private void attack()
+    {
+        spawnAndShootProject("Barrel1");
+        spawnAndShootProject("Barrel2");
+        spawnAndShootProject("Barrel3");
+        spawnAndShootProject("Barrel4");
+        spawnAndShootProject("Barrel5");
+    }
+
+    //spawn and shoot a projectile at the desingated barrel
+    private void spawnAndShootProject(String barrel)
+    {
+        //get projectile from resource folder
+        GameObject projectile = Resources.Load("Ranged_Enemy_Projectile") as GameObject;
+        //get position of spawn point
+        Vector3 barrelVector = GameObject.Find(barrel).transform.position;
+        //instantiate proctile resource folder
+        GameObject projectile_instance = Instantiate(projectile, new Vector3(barrelVector.x, barrelVector.y, 0), Quaternion.identity);
+        //calculate angle to shoot projectile
+        float theta = calculateAngle() * Mathf.Deg2Rad;
+        //calculate the velocity vector of the boi
+        Vector3 vector = new Vector3((float)(10 * Math.Cos(theta)), (float)(10 * Math.Sin(theta)), 0f); //create a vector of x and y velocities
+        //set damage of the projectile
+        projectile_instance.GetComponent<Script_Ranged_Enemy_Projectile>().set_damage(10);
+        //set rotatopm of the projectile
+        projectile_instance.transform.rotation = Quaternion.Euler(0, 0, theta * Mathf.Rad2Deg - 90);
+        //set velocity of the projectile
+        projectile_instance.GetComponent<Rigidbody2D>().velocity = vector;
+    }
+
+    //calculate the angle of rotation paying respect to the offset shift
+    private float calculateAngle()
+    {
+        Vector3 targetPosition = target.transform.position;
+        targetPosition.x = targetPosition.x - this.gameObject.transform.position.x;
+        targetPosition.y = targetPosition.y - this.gameObject.transform.position.y;
+        return (Mathf.Atan2(targetPosition.y, targetPosition.x) * Mathf.Rad2Deg);
+    }
+
+}
