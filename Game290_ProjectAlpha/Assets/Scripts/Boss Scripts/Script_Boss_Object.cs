9a32370df1fe75794f46e1a8a19ca78b87bb2e64
Nathan Ahrendt <nathandfahrendt@gmail.com>
2020-03-14 16:09:57 -0700

    Added framework for boss to be damaged

---
+++ Game290_ProjectAlpha/Assets/Scripts/Boss Scripts/Script_Boss_Object.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Boss_Object : MonoBehaviour
+{
+    private int hitPoints = 1000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //damage boss
+    public void damageBoss(int damage)
+    {
+        hitPoints -= damage;
+        //call destruction/dying animation
+    }
+
+    //called by destruction/dying animation upon completion of animation
+    public void destroyBoss()
+    {
+        Destroy(gameObject);
+    }
+
+}
