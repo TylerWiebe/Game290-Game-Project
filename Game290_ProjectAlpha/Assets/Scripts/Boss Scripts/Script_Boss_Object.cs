716db371bba8e80bd5e7390cf2224110e53e9389
DaneMunday <46771374+DaneMunday@users.noreply.github.com>
2020-03-18 21:25:38 -0700

    Door Spawns on Enemy Death
    - Added Props to level 1

--- Game290_ProjectAlpha/Assets/Scripts/Boss Scripts/Script_Boss_Object.cs
+++ Game290_ProjectAlpha/Assets/Scripts/Boss Scripts/Script_Boss_Object.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class Script_Boss_Object : MonoBehaviour
 {
+    private GameObject doorPrefab;
     private int hitPoints = 1000;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        doorPrefab = Resources.Load("EndGameDoor") as GameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     //damage boss
@@ -29,6 +30,9 @@ public class Script_Boss_Object : MonoBehaviour
     //called by destruction/dying animation upon completion of animation
     public void destroyBoss()
     {
+        //spawn door at position of boss at death
+        Instantiate(doorPrefab, this.transform.position, Quaternion.identity);
+
         Destroy(gameObject);
     }
 
