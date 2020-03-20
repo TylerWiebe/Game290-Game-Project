73ffff4f888666ba9f82b3e08a94c5c815acd0b2
Tyler Wiebe <tyler.d.wiebe@gmail.com>
2020-03-20 16:26:30 -0500

    Revert "Revert "Fixed a combat music bug""
    This reverts commit 0f58618bda6d39390331661cecf6784b9e29096e.

--- Game290_ProjectAlpha/Assets/Scripts/Enemy Scripts/Script_EnemyAI.cs
+++ Game290_ProjectAlpha/Assets/Scripts/Enemy Scripts/Script_EnemyAI.cs
@@ -107,20 +107,20 @@ public class Script_EnemyAI : MonoBehaviour
             //Player has been seen, so operate enemy in aggressive behaviour
             else
             {
-                /*
+                
                 //Music stuff
-                if (needToAddToEnemyCount)
+                if (swapMusicScript != null && needToAddToEnemyCount)
                 {
                     swapMusicScript.alertedEnemiesCount += 1;
                     needToAddToEnemyCount = false;
                 }
                 //change to combat music
-                if ((swapMusicScript.isPlayingCombatMusic == false) & (swapMusicScript.alertedEnemiesCount >= 1))
+                if (swapMusicScript != null && (swapMusicScript.isPlayingCombatMusic == false) & (swapMusicScript.alertedEnemiesCount >= 1))
                 {
                     swapMusicScript.isPlayingCombatMusic = true;
                     swapMusicScript.PlayCombatMusic();
                 }
-                */
+                
 
                 //Start aggressive behaviour
                 StartCoroutine(AggressiveBehaviour());
