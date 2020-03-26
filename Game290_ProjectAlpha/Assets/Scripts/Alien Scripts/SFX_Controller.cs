2e9ceab71123ae5d8e3ba1479162702a78438b9a
Tyler Wiebe <tyler.d.wiebe@gmail.com>
2020-03-26 13:21:55 -0500

    menu SFX implemented

---
+++ Game290_ProjectAlpha/Assets/Scripts/Alien Scripts/SFX_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX_Controller : MonoBehaviour
+{
+    //sfx of the morph
+    //morph volume some value between 0-1
+    public AudioClip morphSFX;
+    public float morphVolume;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void playMorph()
+    {
+        audioSource.PlayOneShot(morphSFX, morphVolume);
+    }
+}
---
+++ Game290_ProjectAlpha/Assets/Scripts/UI/Script_hoverOver_SFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Script_hoverOver_SFX : MonoBehaviour
+{
+    public AudioClip AudioClip;
+    private AudioSource audioSource;
+
+    
+    public void playHover()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.PlayOneShot(AudioClip, 1);
+    }
+
+}
