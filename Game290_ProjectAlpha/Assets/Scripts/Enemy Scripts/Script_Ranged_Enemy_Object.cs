fc299002a9fa3909da0804e314952d110b19a7a1
Nathan Ahrendt <nathandfahrendt@gmail.com>
2020-04-01 16:11:42 -0700

    Ranged guard SFXs fixed/implemented

--- Game290_ProjectAlpha/Assets/Scripts/Enemy Scripts/Script_Ranged_Enemy_Object.cs
+++ Game290_ProjectAlpha/Assets/Scripts/Enemy Scripts/Script_Ranged_Enemy_Object.cs
@@ -11,10 +11,8 @@ public class Script_Ranged_Enemy_Object : MonoBehaviour
     private int spawnChance = 20;
     private Script_SwapMusic swapMusicScript;
 
-
     private GameObject enemy_body;
     private GameObject enemy_attack_cone;
-
 
     //gameobject's audio player
     AudioSource audioSource;
@@ -22,12 +20,21 @@ public class Script_Ranged_Enemy_Object : MonoBehaviour
     //guard hurt sound
     public AudioClip rangedGuardDamagedSFX;
 
+    //guard death sound 1
+    public AudioClip rangedGuardDeath1SFX;
+
+    //guard death sound 2
+    public AudioClip rangedGuardDeath2SFX;
+
     //SFX volume
     public float sfxVolume;
 
     // Start is called before the first frame update
     void Start()
     {
+        //set audioSource to the gameobject's "audio controller"
+        audioSource = GetComponent<AudioSource>();
+
         swapMusicScript = GameObject.Find("Music").GetComponent<Script_SwapMusic>();
     }
 
@@ -47,7 +54,6 @@ public class Script_Ranged_Enemy_Object : MonoBehaviour
         return hit_points;
     }
 
-
     public void set_attack_damage(int attack_damage)
     {
         this.attack_damage = attack_damage;
@@ -60,12 +66,25 @@ public class Script_Ranged_Enemy_Object : MonoBehaviour
 
     public void attacked(float damage)
     {
-        //Debug.Log("GotHit");
+        //play ranged guard damaged sound only when the guard is damaged and will not die, otherwise play death sound
+        if (hit_points - damage > 0)
+            playRangedGuardDamagedSFX();
+        
+        //remove hitpoints from the boss
         hit_points -= damage;
+
+        //if ded
         if (hit_points <= 0)
         {
-            //stop enemy from moving when dead
-            this.transform.GetComponentInParent<Script_EnemyAI>().canMove = false;
+            //play the SFX when the guard dies
+            int random = UnityEngine.Random.Range(1, 3);
+            if (random == 1)
+                playRangedGuardDeath1SFX();
+            else
+                playRangedGuardDeath2SFX();
+
+                //stop enemy from moving when dead
+                this.transform.GetComponentInParent<Script_EnemyAI>().canMove = false;
 
             //set the enemy's velocity to zero when it dies
             this.transform.GetComponentInParent<Script_EnemyAI>().rigidBody.velocity = Vector3.zero;
@@ -81,10 +100,22 @@ public class Script_Ranged_Enemy_Object : MonoBehaviour
         }
     }
 
-    //play slime attacking sound
+    //play ranged guard damaged sound
     public void playRangedGuardDamagedSFX()
     {
         audioSource.PlayOneShot(rangedGuardDamagedSFX, sfxVolume);
+    }
+
+    //play ranged guard death sound 1
+    public void playRangedGuardDeath1SFX()
+    {
+        audioSource.PlayOneShot(rangedGuardDeath1SFX, sfxVolume);
+    }
+
+    //play ranged guard death sound 2
+    public void playRangedGuardDeath2SFX()
+    {
+        audioSource.PlayOneShot(rangedGuardDeath2SFX, sfxVolume);
     }
 
     public void destroy()
